198a12e975e3bc6ac763fa25cef50fb253305a8e
tonnkm13 <tonnkm@gmail.com>
2026-01-10 20:21:02 +0200

    Debug webhook route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
 use App\Http\Controllers\TelegramWebhookController;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/telegram/webhook', function () {
+    Log::debug('INSIDE ROUTE CLOSURE');
+    return response()->json(['ok' => true]);
+});
 
 //Route::post('/telegram/webhook', [TelegramWebhookController::class, 'handle']);
 //Route::get('/ping', function () {
 //   return 'pong';});
 
 
-Route::get('/ping', function () {
-    return 'pong';
-});
+//Route::get('/ping', function () {    return 'pong';});
 
-Route::post('/telegram/webhook', function () {
-    \Log::debug('WEBHOOK ROUTE HIT');
-    return response()->json(['ok' => true]);
-});
+//Route::post('/telegram/webhook', function () {
+ //   \Log::debug('WEBHOOK ROUTE HIT');
+ //   return response()->json(['ok' => true]);});
