68e9234846e2a447fd64f486f7a22f6eb5eb1de1
tonnkm13 <tonnkm@gmail.com>
2026-01-10 19:59:37 +0200

    debug webhook route only

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,16 @@
 use App\Http\Controllers\TelegramWebhookController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/telegram/webhook', [TelegramWebhookController::class, 'handle']);
+//Route::post('/telegram/webhook', [TelegramWebhookController::class, 'handle']);
+//Route::get('/ping', function () {
+//   return 'pong';});
+
+
 Route::get('/ping', function () {
     return 'pong';
 });
 
-
+Route::post('/telegram/webhook', function () {
+    \Log::debug('WEBHOOK ROUTE HIT');
+    return response()->json(['ok' => true]);
+});
