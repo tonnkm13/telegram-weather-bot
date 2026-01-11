ffd354ec9f2ebb7a80c5806dacc040c932815033
tonnkm13 <tonnkm@gmail.com>
2026-01-11 14:21:23 +0200

    return work route to web.php

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,11 @@ use App\Http\Controllers\TelegramWebhookController;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/telegram/webhook', function () {
-    Log::debug('INSIDE ROUTE CLOSURE');
-    return response()->json(['ok' => true]);
-});
+//Route::post('/telegram/webhook', function () {
+ //   Log::debug('INSIDE ROUTE CLOSURE');
+ //   return response()->json(['ok' => true]);});
 
-//Route::post('/telegram/webhook', [TelegramWebhookController::class, 'handle']);
+Route::post('/telegram/webhook', [TelegramWebhookController::class, 'handle']);
 //Route::get('/ping', function () {
 //   return 'pong';});
 
